fa9be59be9e5b2a8d5a43aa3cb02571060681e6f
EliasdsAI <jedantas64@gmail.com>
2026-01-21 21:46:58 -0300

    Add files via upload

---
+++ Nova pasta/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import Cliente
+
+def cadastrar_cliente(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        email = request.POST.get('email')
+        cep = request.POST.get('cep')
+        numero = request.POST.get('numero')
+        compl = request.POST.get('compl')
+
+        if nome and telefone and email and cep and numero and compl:
+
+         cliente = Cliente(nome=nome, telefone=telefone, email=email, cep=cep, numero=numero, compl=compl)
+         cliente.save()
+         return redirect('cadastro_cliente.html')
+
+
+    return render(request, 'cadastrar_cliente.html')
+
+
+
